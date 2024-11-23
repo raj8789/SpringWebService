3e7b38e0da0f37825fd7956577ed4e45284141f1
raj8789 <raushanraj8789@gmail.com>
2024-11-24 00:44:31 +0530

    first commit for chapter 2.1

---
+++ demo-package-for-spring-webservice/src/main/java/com/example/demo/DemoPackageForSpringWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPackageForSpringWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPackageForSpringWebserviceApplication.class, args);
+	}
+
+}
